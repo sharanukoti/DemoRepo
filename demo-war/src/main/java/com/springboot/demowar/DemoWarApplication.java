16f7a34feb714fd7aa244ae12a303583ad335718
Koti <skoti5@dxc.com>
2019-03-08 12:30:07 +0530

    Demo repo for learning purpose.

---
+++ demo-war/src/main/java/com/springboot/demowar/DemoWarApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.demowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoWarApplication extends SpringBootServletInitializer{
+
+public static void main(String[] args) {
+	SpringApplication.run(DemoWarApplication.class, args);
+}
+
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+return application.sources(DemoWarApplication.class);
+}
+
+}
---
+++ demo-war/src/main/java/com/springboot/demowar/config/WebServiceSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.springboot.demowar.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebServiceSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Autowired
+//	private AuthenticationEntryPoint authEntryPoint;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
+				.authenticationEntryPoint(getBasicAuthEntryPoint());
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
+	}
+	
+	@Bean
+	public AuthenticationEntryPoint getBasicAuthEntryPoint() {
+		return new AuthenticationEntryPoint();
+	}
+}
